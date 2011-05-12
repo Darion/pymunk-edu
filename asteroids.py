d8d10cd583c90c14ea40be163dc6942100cf810e
Darion <an.alexander.mail@gmail.com>
2011-05-12 15:40:58 +0600

    В asteroids добавлены границы прямоугольником, чтобы кубики не разлетались за пределы поля

--- asteroids.py
+++ asteroids.py
@@ -13,6 +13,7 @@ class Window(pyglet.window.Window):
         for n in range(1, 80):
             self.add_asteroid()
         self.add_ship()
+        self.lines = self.add_borders()
 
     def init_physics(self):
         pymunk.init_pymunk()
@@ -96,7 +97,7 @@ class Window(pyglet.window.Window):
         return self.space.bodies[len(self.space.bodies)-1]
 
     def update(self):
-        speed = 50;
+        speed = 90;
         if self.keys[key.UP]:
             self.ship_body.apply_impulse((0,speed))
         elif self.keys[key.RIGHT]:
@@ -106,10 +107,41 @@ class Window(pyglet.window.Window):
         elif self.keys[key.LEFT]:
             self.ship_body.apply_impulse((-speed,0))
 
+    def add_borders(self):
+        body = pymunk.Body(pymunk.inf, pymunk.inf)
+        body.position = (50,50)
+        width = 550.0
+        height = 400.0
+        l1 = pymunk.Segment(body, (0.0, 0.0), (0.0, height), 5.0)
+        l2 = pymunk.Segment(body, (0.0, height), (width, height), 5.0)
+        l3 = pymunk.Segment(body, (width, height), (width, 0.0), 5.0)
+        l4 = pymunk.Segment(body, (width, 0), (0.0, 0.0), 5.0)
+        self.space.add_static(l1, l2, l3, l4)
+        return l1,l2,l3,l4
+
+    def draw_lines(self, lines):
+        for line in lines:
+            body = line.body
+            pv1 = body.position + line.a.rotated(body.angle) # 1
+            pv2 = body.position + line.b.rotated(body.angle)
+            p1 = to_pyglet(pv1) # 2
+            p2 = to_pyglet(pv2)
+            pyglet.gl.glColor4f(1.0,0,0,1.0)
+            pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
+                ('v2i', (int(p1.x), int(p1.y), int(p2.x), int(p2.y)))
+            )
+
     def draw(self):
         for asteroid in self.asteroids:
             self.draw_box(asteroid)
         self.draw_box(self.ship, True)
+        self.draw_lines(self.lines)
+
+def to_pyglet(p):
+    return Vec2d(round_to_int(p.x), round_to_int(p.y))
+
+def round_to_int(f):
+    return int(round(f))
 
 if __name__ == "__main__":
     window = Window()
