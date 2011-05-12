0f25c4727e6a1acf95b05d7d0d3e1d3290808260
Darion <an.alexander.mail@gmail.com>
2011-05-12 21:43:06 +0000

    Рефакторинг: часть методов перенесена в классы PygletDraw и PymunkShape

--- asteroids.py
+++ asteroids.py
@@ -1,7 +1,59 @@
+# -*- encoding: utf-8 -*-
+
 import pyglet, random
 from pyglet.window import key
 import pymunk
 from pymunk import Vec2d
+
+
+class PymunkShape():
+    """
+    Общие геометрические фигуры Pymunk
+    """
+    @staticmethod
+    def square(body, side):
+        """
+        :param side: Длина стороны
+        """
+        shape = pymunk.Poly(body,
+            [
+              (0, 0),
+              (side, 0),
+              (side, side),
+              (0, side)
+            ]
+        )
+        return shape
+
+class PygletDraw():
+    """
+    Отрисовка примитивов в Pyglet
+    """
+    @staticmethod
+    def square(coords):
+        """
+        Четырехугольник
+        :param coords: Четыре точки - координаты (Vec2d)
+        """
+        pyglet.graphics.draw(4, pyglet.gl.GL_POLYGON,
+            ('v2i', (
+                int(round(coords[0].x)), int(round(coords[0].y)),
+                int(round(coords[1].x)), int(round(coords[1].y)),
+                int(round(coords[2].x)), int(round(coords[2].y)),
+                int(round(coords[3].x)), int(round(coords[3].y)),
+                ))
+        )
+    @staticmethod
+    def line(p1, p2):
+        """
+        Прямая линия
+        :param p1: Первый конец линии
+        :param p2: Второй конец линии
+        """
+        pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
+                             ('v2i', (int(p1.x), int(p1.y), int(p2.x), int(p2.y)))
+                             )
+
 
 class SpaceShip():
     def __init__(self, space):
@@ -12,30 +64,14 @@ class SpaceShip():
         x = random.randint(20,600)
         y = random.randint(20,400)
         body.position = x, y
-        shape = pymunk.Poly(body,
-                            [
-                              (0, 0),
-                              (radius, 0),
-                              (radius, radius),
-                              (0, radius)
-                            ]
-                          )
+        shape = PymunkShape.square(body, radius)
         space.add(body, shape)
         self.shape = shape
         self.body = body
     def draw_box(self, box):
         coords = box.get_points()
-        r = random.random()
-        g = random.random()
         pyglet.gl.glColor4f(1.0, 0.0, 0.0, 0.0)
-        pyglet.graphics.draw(4, pyglet.gl.GL_POLYGON,
-            ('v2i', (
-                int(round(coords[0].x)), int(round(coords[0].y)),
-                int(round(coords[1].x)), int(round(coords[1].y)),
-                int(round(coords[2].x)), int(round(coords[2].y)),
-                int(round(coords[3].x)), int(round(coords[3].y)),
-                ))
-        )
+        PygletDraw.square(coords)
     def draw(self):
         self.draw_box(self.shape)
 
@@ -82,35 +118,16 @@ class Window(pyglet.window.Window):
         x = random.randint(20,600)
         y = random.randint(20,400)
         body.position = x, y
-        shape = pymunk.Poly(body,
-                            [
-                              (0, 0),
-                              (radius, 0),
-                              (radius, radius),
-                              (0, radius)
-                            ]
-                          )
+        shape = PymunkShape.square(body, radius)
         self.space.add(body, shape)
         return shape
 
-
-
-    def draw_box(self, box, ship=False):
+    def draw_box(self, box):
         coords = box.get_points()
         r = random.random()
         g = random.random()
-        if(ship):
-            pyglet.gl.glColor4f(1.0, 0.0, 0.0, 0.0)
-        else:
-            pyglet.gl.glColor4f(r, g, 1.0, 1.0)
-        pyglet.graphics.draw(4, pyglet.gl.GL_POLYGON,
-            ('v2i', (
-                int(round(coords[0].x)), int(round(coords[0].y)),
-                int(round(coords[1].x)), int(round(coords[1].y)),
-                int(round(coords[2].x)), int(round(coords[2].y)),
-                int(round(coords[3].x)), int(round(coords[3].y)),
-                ))
-        )
+        pyglet.gl.glColor4f(r, g, 1.0, 1.0)
+        PygletDraw.square(coords)
 
     def update(self):
         speed = 90;
@@ -135,17 +152,16 @@ class Window(pyglet.window.Window):
         self.space.add_static(l1, l2, l3, l4)
         return l1,l2,l3,l4
 
+
     def draw_lines(self, lines):
         for line in lines:
             body = line.body
-            pv1 = body.position + line.a.rotated(body.angle) # 1
+            pv1 = body.position + line.a.rotated(body.angle)
             pv2 = body.position + line.b.rotated(body.angle)
-            p1 = to_pyglet(pv1) # 2
+            p1 = to_pyglet(pv1)
             p2 = to_pyglet(pv2)
             pyglet.gl.glColor4f(0.0,1.0,1.0,1.0)
-            pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
-                ('v2i', (int(p1.x), int(p1.y), int(p2.x), int(p2.y)))
-            )
+            PygletDraw.line(p1, p2)
 
     def draw(self):
         for asteroid in self.asteroids:
