6f90d1464ad650dfa9da8090a9c182e6d8340948
Darion <an.alexander.mail@gmail.com>
2011-05-24 18:13:21 +0600

    Отрисовка объекта, накликанного мышкой, в pyglet. В процессе отладки.

--- sandbox/window.py
+++ sandbox/window.py
@@ -1,16 +1,17 @@
 import pyglet
 from pyglet.window import key
+from pyglet.window import mouse
 import pymunk
 from pymunk import Vec2d
 from draw import PygletDraw
 
 class Window(pyglet.window.Window):
-
+    objects = []
+    drawline = None
     def __init__(self, *args, **kwargs):
         pyglet.window.Window.__init__(self, *args, **kwargs)
         self.init_physics()
         self.init_handlers()
-        self.drawline = DrawLines()
 
     def init_physics(self):
         pymunk.init_pymunk()
@@ -27,25 +28,55 @@ class Window(pyglet.window.Window):
             self.clear()
             self.update()
             self.space.step(1/50.0)
-            print len(self.drawline.points)
             self.draw()
             pyglet.clock.tick()
             self.flip()
 
     def on_mouse_press(self, x, y, button, modifiers):
-        self.drawline.add_point(Vec2d(x, y))
+        if self.drawline != None:
+            self.drawline.add_point(Vec2d(x, y))
+            if button == mouse.LEFT:
+                pass
+            elif button == mouse.RIGHT:
+                self.objects.append(Polygonal(self.drawline.points))
+                self.drawline = None
+        else:
+            self.drawline = DrawLines()
+            self.drawline.add_point(Vec2d(x, y))
+
 
     def on_mouse_motion(self, x, y, dx, dy):
-        self.drawline.set_float_point(Vec2d(x, y))
+        if self.drawline != None:
+            self.drawline.set_float_point(Vec2d(x, y))
 
     def update(self):
+        print self.drawline
         pass
     def draw(self):
-        self.drawline.draw()
+        if self.drawline != None:
+            self.drawline.draw()
+        for o in self.objects:
+            o.draw()
+
+class PhysObject():
+    def __init__(self):
+        pass
+    def draw(self):
+        pass
+
+class Polygonal(PhysObject):
+    def __init__(self, points):
+        self.points = points
+    def draw(self):
+        ns = []
+        for point in self.points:
+            ns.append(point.x)
+            ns.append(point.y)
+        pyglet.graphics.draw(len(self.points), pyglet.gl.GL_POLYGON, ('v2f', ns))
 
 class DrawLines():
     points = []
-    float_point = []
+    float_point = Vec2d(0,0)
     def __init__(self):
         pass
     def add_point(self, point):
@@ -56,8 +87,10 @@ class DrawLines():
         pass
     def draw(self):
         if len(self.points) > 1:
+            print 'points > 1'
             last = self.points[0]
             for n in range(1, len(self.points)):
+                print 'n=%s' % n
                 current = self.points[n]
                 PygletDraw.line(last, current)
                 last = current
